e51fdf002bcb687a6caaa721e2076922ac7d11f2
JhonatanMarcilio <jhonatan.marcilio@alunos.afya.com.br>
2025-04-25 21:46:20 -0400

    Criando alguns componentes - 25/04/2025

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import ContactItem from "./components/ContactItem";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const contacts = [
+    {
+      id: 1,
+      nome: "João",
+      phone: "(69) 99310-0703",
+      mail: "exemplo@exemplo.com.br",
+    },
+    {
+      id: 2,
+      nome: "Maria",
+      phone: "(69) 22222-2222",
+      mail: "exemplo2@exemplo.com.br",
+    },
+    {
+      id: 3,
+      nome: "José",
+      phone: "(69) 33333-3333",
+      mail: "exemplo3@exemplo.com.br",
+    },
+  ];
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Agenda de Contatos</h1>
+      <ContactItem></ContactItem>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/ContactItem.jsx
@@ -0,0 +1,15 @@
+function ContactItem({ contacts }) {
+  return (
+    <>
+      <h2>Jhonatan Marcílio</h2>
+      <p>(69) 99310-0703</p>
+      <p>jhonatan.marcilio@alunos.afya.com.br</p>
+      <div>
+        <button>Editar</button>
+        <button>Deletar</button>
+      </div>
+    </>
+  );
+}
+
+export default ContactItem;
