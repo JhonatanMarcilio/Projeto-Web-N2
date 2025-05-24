e670bccbd5e761959ecc24f0a9f2fe332759c272
JhonatanMarcilio <jhonatan.marcilio@alunos.afya.com.br>
2025-05-23 21:45:21 -0400

    Criando o Form para adicionar dados ao bd e mostrar na lista

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import { useEffect, useState } from "react";
 import "./App.css";
 import ContactList from "./components/ContactList";
+import ContactForm from "./components/contactForm";
 
 function App() {
   const [contacts, setContacts] = useState([]);
+  const [showForm, setShowForm] = useState(false);
 
   useEffect(() => {
     fetch("http://localhost:3001/contacts")
@@ -18,9 +20,35 @@ function App() {
       });
   }, []);
 
+  function addContact(newContact) {
+    fetch("http://localhost:3001/contacts", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(newContact),
+    })
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (data) {
+        setContacts([...contacts, data]);
+        setShowForm(false); // Esconde o form depois de adicionar
+      })
+      .catch(function (error) {
+        console.error("Erro ao adicionar um contato: ", error);
+      });
+  }
+
   return (
     <div className="container">
       <h1>Agenda de Contatos</h1>
+      {/* Botão para mostrar o from */}
+      {!showForm && (
+        <button onClick={() => setShowForm(true)}>Novo Contato</button>
+      )}
+      {/* Form visível apenas se showForm for true */}
+      {showForm && (
+        <ContactForm onAdd={addContact} onClose={() => setShowForm(true)} />
+      )}
       <ContactList contacts={contacts} />
     </div>
   );
---
+++ src/components/ContactForm.jsx
@@ -0,0 +1,58 @@
+import { useState } from "react";
+import "./ContactForm.css";
+
+function ContactForm({ onAdd, onClose }) {
+  const [name, setName] = useState("");
+  const [phone, setPhone] = useState("");
+  const [email, setEmail] = useState("");
+
+  // Evita que a pagina seja recarregada.
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    const newContact = {
+      name: name,
+      phone: phone,
+      email: email,
+    };
+
+    onAdd(newContact);
+
+    // Limpa os inputs
+    setName("");
+    setPhone("");
+    setEmail("");
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+        placeholder="Nome"
+        required
+      />
+      <input
+        type="tel"
+        value={phone}
+        onChange={(e) => setPhone(e.target.value)}
+        placeholder="Fone"
+        required
+      />
+      <input
+        type="email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        placeholder="E-Mail"
+        required
+      />
+      <button type="submit">Adicionar</button>
+      <button type="button" onClick={onClose}>
+        Cancelar
+      </button>
+    </form>
+  );
+}
+
+export default ContactForm;
